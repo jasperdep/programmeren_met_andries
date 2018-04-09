c73ef5e2fc1749e2dac81318c4e6635908c132c3
poot0018 <poot0018@hz.nl>
2018-04-09 12:34:53 +0200

    Eerst commit

---
+++ Test/Test/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int i = 123;
+            object o = (object)i;
+
+            Console.WriteLine(o);
+
+            int age = 11;
+            Person piet = new Person();
+
+            int myAge = age;
+
+            Person sjaak = piet;
+            piet = null;
+
+            Console.WriteLine(sjaak.height);
+            Console.ReadLine();
+            
+        }
+
+    }
+    class Person
+    {
+        public int height = 2000;
+    }
+}
